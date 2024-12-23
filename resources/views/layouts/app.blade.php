8178c02af80dd9695543f1e86e9cf1fea9a9aa01
FelisMaillard <151616377+FelisMaillard@users.noreply.github.com>
2024-12-23 22:58:54 +0100

    navbar fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -241,9 +241,9 @@
                     </svg>
                 </a>
 
-                <a href="{{ route('notifications.index') }}" class="relative flex items-center p-2 hover:bg-gray-800 rounded-lg transition-colors duration-200">
+                <a href="{{ route('notifications.index') }}" class="relative flex items-center rounded-lg">
                     <div class="w-6 flex justify-center relative">
-                        <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg class="w-6 h-6 text-white hover:text-purple-500 transition-colors duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                         </svg>
                         @if($pendingRequestsCount > 0)
