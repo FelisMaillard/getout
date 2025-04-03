646e466086acbbc20a6a7f811705041ec156f27c
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2025-04-03 09:15:51 +0200

    message changes

--- resources/views/channels/_messages.blade.php
+++ resources/views/channels/_messages.blade.php
@@ -128,7 +128,7 @@
               class="flex space-x-4">
             @csrf
 
-            {{-- Upload button --}}
+            {{-- Upload button
             <label class="p-2 text-gray-400 hover:text-white transition-colors rounded-lg hover:bg-gray-800 cursor-pointer flex items-center justify-center">
                 <input type="file"
                        name="file"
@@ -139,7 +139,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                 </svg>
-            </label>
+            </label> --}}
 
             <div class="flex-1 relative">
                 <textarea
@@ -149,12 +149,6 @@
                     class="w-full px-4 py-2 max-h-32 bg-gray-900 border border-gray-800 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent resize-none overflow-auto"
                     placeholder="Envoyer un message..."
                     autocomplete="off"></textarea>
-
-                <div id="emoji-button" class="absolute right-2 bottom-2 text-gray-400 hover:text-white cursor-pointer p-1">
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                    </svg>
-                </div>
             </div>
 
             {{-- Send button --}}
